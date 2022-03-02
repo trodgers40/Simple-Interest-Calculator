2d8cbab09d5f9219c56380a02f45335264a1d809
Thomas <tomrodger22@gmail.com>
2022-03-02 21:17:32 +0000

    Changes in Theia Lab

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
+
+
 function compute()
 {
-    var p = document.getElementById("principal").nodeValue;
-    var rate = document.getElementById("rate").nodeValue;
-    var years = document.getElementById("years").nodeValue;
+    var p = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     var interest = (p * years * rate) / 100;
     var years = new Date().getFullYear()+parseInt(years);
 
-    if(p.nodeValue == "")
+    if(p== "")
     {
         alert('Please fill out Amount');
         p.focus();
         return false;
     }
-    if(p.nodeValue <=0)
+    if(p<=0)
     {
         alert("Amount must be greater than Zero ");
         p.focus();
@@ -20,10 +27,4 @@ function compute()
     }
     document.getElementById("result").innerHTML = "If you deposit " + p + ", \<br> at an interest rate of " + rate + "%. \<br> You will receive an amount of " + interest + ",\<br> in year " + years
     return true;
-}
-
-function updateRate()
-{
-    var rateval = document.getElementById("rate").nodeValue;
-    document.getElementById("rate_val").innerText = rateval;
 }
